4310843effada0c509511fc7900f4a5caa3984ed
jinyan123 <18879230144@163.com>
2020-06-04 20:32:37 +0800

    jy

---
+++ thread-demo/src/main/java/com/gupaoedu/vip/spring/demo/jinyan/Config.java
@@ -0,0 +1,13 @@
+package com.gupaoedu.vip.spring.demo.jinyan;
+
+import lombok.Data;
+
+@Data
+public class Config {
+
+    private String className;
+
+    private String beanName;
+
+    private String courseName;
+}
