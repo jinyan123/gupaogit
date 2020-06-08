fb265dff0202f642ad169d7835ca5362a3d30848
jinyan123 <18879230144@163.com>
2020-06-08 16:06:22 +0800

    jj

--- thread-demo/src/main/java/com/gupaoedu/vip/spring/demo/jinyan/TestClass.java
+++ thread-demo/src/main/java/com/gupaoedu/vip/spring/demo/jinyan/TestClass.java
@@ -1,8 +1,8 @@
 package com.gupaoedu.vip.spring.demo.jinyan;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.concurrent.TimeUnit;
 
 public class TestClass {
     public static void main(String[] args) {
@@ -15,8 +15,8 @@ public class TestClass {
             //  System.out.println(list);
         }
         long start = System.nanoTime();
-        int count = (int) list.stream().sequential().sorted().count();
-        // int count  = (int) list.stream().parallel().sorted().count();
+        // int count = (int) list.stream().sequential().sorted().count();
+        int count  = (int) list.stream().parallel().sorted().count();
         long end = System.nanoTime();
         long ms = TimeUnit.NANOSECONDS.toMillis(end - start);
         System.out.println(ms + "ms");
